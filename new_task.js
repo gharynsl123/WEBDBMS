f0ce6202c4266e323a7ec1ca8c820bc7ed02df1e
Harin <57610449+gharynsl123@users.noreply.github.com>
2022-01-27 14:16:08 +0700

    change commit

---
+++ new_task.js
@@ -0,0 +1,16 @@
+let kendaraan = {
+    kendaraan : {
+        mobil : "peungeot",
+        motor : "v-xion",
+        seepeda : "united"
+    },
+    jalan : "jalan berkah",
+    kota : "bekasi",
+    platNomor : "jabodetabek",
+    kereta : {
+        namaKa : "argo Bromo anggrek",
+        tujuan : "senen - pasar turi"
+    }
+}
+
+console.table(kendaraan)
